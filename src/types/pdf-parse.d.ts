51d123d210913747c8dc5f3b1a7f445245c0b41e
Harchana <cyberg7.llp@gmail.com>
2025-08-06 12:30:22 +0800

    Remove old project files, keep only BuilderKit-Pro structure

--- src/types/pdf-parse.d.ts
+++
@@ -1,8 +0,0 @@
-// This is a type definition file for the pdf-parse library.
-
-declare module 'pdf-parse/lib/pdf-parse.js' {
-  import { PDFParseOptions, PDFParseResult } from 'pdf-parse';
-
-  function pdfParse(dataBuffer: Buffer | Blob, options?: PDFParseOptions): Promise<PDFParseResult>;
-  export = pdfParse;
-}
